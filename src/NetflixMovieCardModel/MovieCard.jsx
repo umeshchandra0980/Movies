ace6642f46ca7639cfd98ed4e793935934f4944f
umesh <venkata2417@gmail.com>
2025-09-01 15:07:07 +0530

    Fix missing MovieCard.css import

--- src/NetflixMovieCardModel/MovieCard.jsx
+++ src/NetflixMovieCardModel/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import "./MovieCard.css";
+// import "./movieCard.css";
 import Radio from "../Loders/Radio";
 
 export default function MovieCard({ movie }) {
